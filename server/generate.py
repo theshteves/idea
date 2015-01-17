36345f2aff7c9f9581a6ad0d207676a8afaa8b7b
samkling <klingtennis@gmail.com>
2015-01-17 13:35:29 -0500

    initial generator

--- server/generate.py
+++ server/generate.py
@@ -1,3 +1,23 @@
 
 import json
-print("hey")
+import random
+
+rand_list = random.sample(range(1,638),4)
+
+#opens personality traits file
+fp = open("traits.txt","r")
+
+count = 0
+count_trt = 0
+next = 0
+
+#finds traits
+while count_trt < 4:
+	for trait in fp:
+		if count in rand_list:
+			print(trait.strip())
+			next = 1
+			count_trt += 1
+		if count_trt == 4:
+			break
+		count += 1
