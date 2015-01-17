1af74648e256aba907cbbab2a7489021dadf99d4
samkling <klingtennis@gmail.com>
2015-01-17 05:06:33 -0500

    new commit

---
+++ new_idea/new_idea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new_idea
+//
+//  Created by Sam Kling on 1/17/15.
+//  Copyright (c) 2015 Sam Kling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
