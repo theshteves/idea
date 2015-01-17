d029eb42c1b5d77c2877b9c5b6e2afd01c51890a
samkling <klingtennis@gmail.com>
2015-01-17 14:42:42 -0500

    edited function

--- server/generate.py
+++ server/generate.py
@@ -2,22 +2,30 @@
 import json
 import random
 
-rand_list = random.sample(range(1,638),4)
+def generator(doc,amount,total):
+	rand_list = random.sample(range(1,total),amount)
 
-#opens personality traits file
-fp = open("traits.txt","r")
+	#opens personality traits file
+	fp = open(doc,"r")
 
-count = 0
-count_trt = 0
-next = 0
+	count = 0
+	count_trt = 0
+	a_list = []
 
-#finds traits
-while count_trt < 4:
-	for trait in fp:
-		if count in rand_list:
-			print(trait.strip())
-			next = 1
-			count_trt += 1
-		if count_trt == 4:
-			break
-		count += 1
+	#finds traits
+	while count_trt < amount:
+		for trait in fp:
+			if count in rand_list:
+				a_list.append(trait.strip())
+				count_trt += 1
+			if count_trt == amount:
+				break
+			count += 1
+	return a_list
+
+
+traits = generator("traits.txt",4,638)
+#first_name = generator("firstname.txt",1,)
+#last_name = generator("lastname.txt",1)
+
+print(traits)
